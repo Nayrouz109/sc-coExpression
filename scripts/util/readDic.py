cebcef02900dde53f54c54bcac87fd5048e3c564
Nayrouz109 <nayrouz@student.ubc.ca>
2026-01-02 16:46:20 -0800

    stop tracking extra scripts folders; update .gitignore

--- scripts/util/readDic.py
+++
@@ -1,9 +0,0 @@
-
-
-### using json module 
-import json
-
-gene_dict_path = '/home/nelazzabi/rewiring/data/genes/filtered_genes.json'
-with open(gene_dict_path, "r") as fp: 
-    gene_dict = json.load(fp) 
-
